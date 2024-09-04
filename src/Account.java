7a531c6e9ca31ea0af33773fe4bc53b7d701ea1c
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-09-04 17:28:41 +0300

    The 'Bank' class has been coded.

--- src/Account.java
+++ src/Account.java
@@ -4,20 +4,16 @@ public class Account {
     static private String accountNumber;
     static private String owner;
     static private double balance = 0;
-
     static public List<Account> accounts;
-
     public Account(String accountNumber, String owner, double balance){
         this.accountNumber = accountNumber;
         this.owner = owner;
         this.balance = balance;
     }
-
     public void deposit(double amount){
         this.balance += amount;
         viewBalance();
     }
-
     public void withdraw(double amount){
         if (this.balance - amount < 0){
             System.out.println("\nYour balance is insufficient.\n");
@@ -27,7 +23,6 @@ public class Account {
         }
         viewBalance();
     }
-
     public void viewBalance(){
         System.out.println("*".repeat(50));
         System.out.println("\nBalance: " + this.balance + "\n");
---
+++ src/Bank.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.util.List;
+
+public class Bank {
+    public static List<String> transactionHistory;
+    public static void displayBankInfo(){
+        System.out.println("*".repeat(50));
+        System.out.println("\n\nName: BJK BANK");
+        System.out.println("Established: 2024");
+        System.out.println("Services: Depositing money, withdrawing money, account opening");
+        System.out.println("Contact: 0450 565 56");
+    }
+    public static void trackTransaction(String description){
+        LocalDate date = LocalDate.now();
+        transactionHistory.add(date + " " + description);
+    }
+
+}
