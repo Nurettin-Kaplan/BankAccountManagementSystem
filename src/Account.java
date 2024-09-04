d1bd2e2fe8f00e1e72845dbafdb6e21bd8b342b6
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-09-04 12:37:13 +0300

    The coding of the 'Account' class was completed.

---
+++ src/Account.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+public class Account {
+    static private String accountNumber;
+    static private String owner;
+    static private double balance = 0;
+
+    static public List<Account> accounts;
+
+    public Account(String accountNumber, String owner, double balance){
+        this.accountNumber = accountNumber;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount){
+        this.balance += amount;
+        viewBalance();
+    }
+
+    public void withdraw(double amount){
+        if (this.balance - amount < 0){
+            System.out.println("\nYour balance is insufficient.\n");
+        }
+        else{
+            this.balance -= amount;
+        }
+        viewBalance();
+    }
+
+    public void viewBalance(){
+        System.out.println("*".repeat(50));
+        System.out.println("\nBalance: " + this.balance + "\n");
+        System.out.println("*".repeat(50));
+    }
+}
