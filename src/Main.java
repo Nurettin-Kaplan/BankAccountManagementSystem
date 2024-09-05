c55afffef1a5b04afe734b9a86b551493325aa8c
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-09-05 14:00:40 +0300

    The deposit features for accounts were coded.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,33 @@
 import java.util.Scanner;
-
 public class Main {
     public static int choice;
     public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
 
-        Account account1 = new Account("1", "Nurettin",85250);
-        Account account2 = new Account("2", "Asli",56500);
-        Account account3 = new Account("3", "Clara",24100);
+        System.out.print("\nEnter your name: ");
+        String name = input.nextLine();
 
-        Account.accounts.add(account1);
-        Account.accounts.add(account2);
-        Account.accounts.add(account3);
+        System.out.print("Enter a new account number: ");
+        String number = input.nextLine();
+
+        Account newAccount = new Account(number, name, 0);
+        Account.accounts.add(newAccount);
 
         while(true){
-            Menu1();
+            Menu();
             switch (choice){
                 case 1:
-                    NewCreateAccount();
+                    newAccount.viewBalance();
                     break;
                 case 2:
+                    Deposit(newAccount);
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+                    break;
+                case 5:
                     break;
                 default:
                     System.out.println("\nInvalid entry. Please try again.");
@@ -27,11 +35,22 @@ public class Main {
             }
         }
     }
-    public static void Menu1(){
-        System.out.println("\n\n1- Create an account.");
-        System.out.println("2- Log in to your account.");
+    public static void Menu(){
+        System.out.println("\n\n1- Balance Inquiry");
+        System.out.println("2- Deposit Money");
+        System.out.println("3-Withdraw Money");
+        System.out.println("4-View bank information");
+        System.out.println("5-View transaction history");
         System.out.print("Enter your choice: ");
         choice = input.nextInt();
+        input.nextLine();
     }
-    
+
+    public static void Deposit(Account account){
+        System.out.print("\nEnter the amount you want to deposit: ");
+        double deposit = input.nextDouble();
+
+        account.deposit(deposit);
+        Bank.trackTransaction("The deposit transaction was completed successfully.");
+    }
 }
